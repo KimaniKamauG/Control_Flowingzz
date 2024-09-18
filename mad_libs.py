607da097bb87f030406e6f58295a91cace822334
KimaniKamauG <143401342+KimaniKamauG@users.noreply.github.com>
2024-09-18 21:13:11 +0300

    Created a script that allows the user to input noun, adjevtive and verbs.

---
+++ mad_libs.py
@@ -0,0 +1,5 @@
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+
+print(f"On a beautiful {noun} day, I went to the zoo. I saw a funny {adjective} monkey swinging from the trees. Then, I spotted a majestic {adjective} lion lounging in the sun. What a wild and {verb} experience!")
